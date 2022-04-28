e3a42444d5df0f4937fe976f630c6bcd3824a540
Gamerize <gundam.issacwai@gmail.com>
2022-04-28 14:13:04 +0100

    Added slashing

--- Assets/Script/Slicer.cs
+++ Assets/Script/Slicer.cs
@@ -1,95 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Rendering;
-
-namespace Assets.Scripts
+﻿using UnityEngine;
+using EzySlice;
+public class Slicer : MonoBehaviour
 {
-    class Slicer
+    public Material materialAfterSlice;
+    public LayerMask sliceMask;
+    public bool isTouched;
+
+    private void Update()
     {
-        /// <summary>
-        /// Slice the object by the plane 
-        /// </summary>
-        /// <param name="plane"></param>
-        /// <param name="objectToCut"></param>
-        /// <returns></returns>
-        public static GameObject[] Slice(Plane plane, GameObject objectToCut)
-        {            
-            //Get the current mesh and its verts and tris
-            Mesh mesh = objectToCut.GetComponent<MeshFilter>().mesh;
-            var a = mesh.GetSubMesh(0);
-            Sliceable sliceable = objectToCut.GetComponent<Sliceable>();
-
-            if(sliceable == null)
-            {
-                throw new NotSupportedException("Cannot slice non sliceable object, add the sliceable script to the object or inherit from sliceable to support slicing");
-            }
-            
-            //Create left and right slice of hollow object
-            SlicesMetadata slicesMeta = new SlicesMetadata(plane, mesh, sliceable.IsSolid, sliceable.ReverseWireTriangles, sliceable.ShareVertices, sliceable.SmoothVertices);            
-
-            GameObject positiveObject = CreateMeshGameObject(objectToCut);
-            positiveObject.name = string.Format("{0}_positive", objectToCut.name);
-
-            GameObject negativeObject = CreateMeshGameObject(objectToCut);
-            negativeObject.name = string.Format("{0}_negative", objectToCut.name);
-
-            var positiveSideMeshData = slicesMeta.PositiveSideMesh;
-            var negativeSideMeshData = slicesMeta.NegativeSideMesh;
-
-            positiveObject.GetComponent<MeshFilter>().mesh = positiveSideMeshData;
-            negativeObject.GetComponent<MeshFilter>().mesh = negativeSideMeshData;
-
-            SetupCollidersAndRigidBodys(ref positiveObject, positiveSideMeshData, sliceable.UseGravity);
-            SetupCollidersAndRigidBodys(ref negativeObject, negativeSideMeshData, sliceable.UseGravity);
-
-            return new GameObject[] { positiveObject, negativeObject};
-        }        
-
-        /// <summary>
-        /// Creates the default mesh game object.
-        /// </summary>
-        /// <param name="originalObject">The original object.</param>
-        /// <returns></returns>
-        private static GameObject CreateMeshGameObject(GameObject originalObject)
+        if (isTouched == true)
         {
-            var originalMaterial = originalObject.GetComponent<MeshRenderer>().materials;
+            isTouched = false;
 
-            GameObject meshGameObject = new GameObject();
-            Sliceable originalSliceable = originalObject.GetComponent<Sliceable>();
+            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+            
+            foreach (Collider objectToBeSliced in objectsToBeSliced)
+            {
+                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
 
-            meshGameObject.AddComponent<MeshFilter>();
-            meshGameObject.AddComponent<MeshRenderer>();
-            Sliceable sliceable = meshGameObject.AddComponent<Sliceable>();
+                GameObject upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+                GameObject lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
 
-            sliceable.IsSolid = originalSliceable.IsSolid;
-            sliceable.ReverseWireTriangles = originalSliceable.ReverseWireTriangles;
-            sliceable.UseGravity = originalSliceable.UseGravity;
+                upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+                lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
 
-            meshGameObject.GetComponent<MeshRenderer>().materials = originalMaterial;
+                MakeItPhysical(upperHullGameobject);
+                MakeItPhysical(lowerHullGameobject);
 
-            meshGameObject.transform.localScale = originalObject.transform.localScale;
-            meshGameObject.transform.rotation = originalObject.transform.rotation;
-            meshGameObject.transform.position = originalObject.transform.position;
-
-            meshGameObject.tag = originalObject.tag;
-
-            return meshGameObject;
-        }
-
-        /// <summary>
-        /// Add mesh collider and rigid body to game object
-        /// </summary>
-        /// <param name="gameObject"></param>
-        /// <param name="mesh"></param>
-        private static void SetupCollidersAndRigidBodys(ref GameObject gameObject, Mesh mesh, bool useGravity)
-        {                     
-            MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
-            meshCollider.sharedMesh = mesh;
-            meshCollider.convex = true;
-
-            var rb = gameObject.AddComponent<Rigidbody>();
-            rb.useGravity = useGravity;
+                Destroy(objectToBeSliced.gameObject);
+            }
         }
     }
+
+    private void MakeItPhysical(GameObject obj)
+    {
+        obj.AddComponent<MeshCollider>().convex = true;
+        obj.AddComponent<Rigidbody>();
+    }
+
+    private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
+    {
+        return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+    }
+
+
 }
